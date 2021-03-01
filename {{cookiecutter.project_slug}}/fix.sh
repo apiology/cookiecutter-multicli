4903e8bcf60dbee7a7612a70dd8b74b29c240324
Vince Broz <vince@broz.cc>
2021-03-01 15:53:16 -0500

    Try set +u

--- {{cookiecutter.project_slug}}/fix.sh
+++ {{cookiecutter.project_slug}}/fix.sh
@@ -35,7 +35,10 @@ ensure_node_versions() {
 }
 
 ensure_npm_modules() {
+  # https://app.circleci.com/pipelines/github/apiology/cookiecutter-chrome-extension/67/workflows/ae764563-1f72-49d1-a663-a6cc6e64060a/jobs/88
+  set +u
   npm install
+  set -u
 }
 
 install_rbenv() {
