3a7d5271e3fbca32b23a38bf8fc7eea1fc37b062
Vince Broz <vince@broz.cc>
2021-03-01 16:03:30 -0500

    Let's see if this is necessary

--- {{cookiecutter.project_slug}}/fix.sh
+++ {{cookiecutter.project_slug}}/fix.sh
@@ -7,13 +7,11 @@ install_nvm() {
 }
 
 set_nvm_env_variables() {
-  set +eu
   export NVM_DIR="$HOME/.nvm"
   # shellcheck disable=SC1090
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
   # shellcheck disable=SC1090
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-  set -eu
 }
 
 ensure_nvm() {
