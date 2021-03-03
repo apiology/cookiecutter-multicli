cd310e1f486d8e9f493605c15d1bba376f27ab61
Vince Broz <vince@broz.cc>
2021-03-03 08:49:32 -0500

    Fill out OmniBox example code

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.js
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.js
@@ -1,11 +1,25 @@
-const doWork = (tab) => {
-  // No tabs or host permissions needed!
-  console.log(`Turning ${tab.url} red!`);
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"',
-  });
+// How on God's green earth is there no built-in function to do this?
+//
+// https://stackoverflow.com/questions/40263803/native-javascript-or-es6-way-to-encode-and-decode-html-entities
+const escapeHTML = (str) => str.replace(/[&<>'"]/g,
+  (tag) => ({
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    "'": '&#39;',
+    '"': '&quot;',
+  }[tag]));
+
+const pullSuggestions = (text) => [{
+  content: `some input data for next step, maybe containing ${text}`,
+  description: escapeHTML('some human readable text'),
+}];
+
+const actOnInputData = (text) => {
+  console.log(`Acting upon ${text}`);
 };
 
 module.exports = {
-  doWork,
+  pullSuggestions,
+  actOnInputData,
 };
