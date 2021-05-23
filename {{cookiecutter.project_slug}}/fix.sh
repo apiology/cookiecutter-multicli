231de0bcaa1a9c834080570b0d4cc016439713de
Vince Broz <vince@broz.cc>
2021-05-22 22:22:52 -0400

    Address shellcheck error

--- {{cookiecutter.project_slug}}/fix.sh
+++ {{cookiecutter.project_slug}}/fix.sh
@@ -14,9 +14,9 @@ set_nvm_env_variables() {
   #
   set +e
   export NVM_DIR="$HOME/.nvm"
-  # shellcheck disable=SC1090
+  # shellcheck disable=SC1090,SC1091
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-  # shellcheck disable=SC1090
+  # shellcheck disable=SC1090,SC1091
   [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
   set -e
 }
