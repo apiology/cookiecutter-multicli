9b06f4e93251c40d4fee75696cba57dfaca40f7b
Vince Broz <vince@broz.cc>
2021-12-04 10:11:38 -0500

    Update webpack config

--- {{cookiecutter.project_slug}}/webpack.config.js
+++ {{cookiecutter.project_slug}}/webpack.config.js
@@ -23,19 +23,7 @@ module.exports = {
   output: {
     filename: '[name].js',
   },
-  node: {
-    // https://github.com/webpack-contrib/css-loader/issues/447
-    //
-    // ERROR in ./node_modules/asana/lib/auth/index.js
-    // Module not found: Error: Can't resolve './auto_detect' in
-    //   '/Users/broz/src/upvoter_for_asana/node_modules/asana/lib/auth'
-    // @ ./node_modules/asana/lib/auth/index.js 6:21-45
-    // @ ./node_modules/asana/index.js
-    // @ ./src/upvoter.js
-    // @ multi ./src/background.js ./src/upvoter.js
-    fs: 'empty',
-  },
+  // 'inline-source-map' is suggested by https://webpack.js.org/guides/typescript/
   // 'cheap-module-source-map' is suggested by https://stackoverflow.com/questions/48047150/chrome-extension-compiled-by-webpack-throws-unsafe-eval-error
-
   devtool: 'cheap-module-source-map',
 };
