64367ea180e08d1d207d63f309a155a267b6d19c
Vince Broz <vince@broz.cc>
2022-06-08 08:32:07 -0400

    Set platform object in background.ts (#122)

--- {{cookiecutter.project_slug}}/src/chrome-extension/background.ts
+++ {{cookiecutter.project_slug}}/src/chrome-extension/background.ts
@@ -4,7 +4,11 @@
  * Initialized when browser extension is loaded.
  */
 
+import { setPlatform } from '../platform.js';
+import ChromeExtensionPlatform from './chrome-extension-platform.js';
 import { doWork } from '../{{cookiecutter.project_slug}}.js';
+
+setPlatform(new ChromeExtensionPlatform());
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(doWork);
