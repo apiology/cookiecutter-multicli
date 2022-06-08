4bd9a55c6e4d6ed936726fa224ee4287613844bb
Vince Broz <vince@broz.cc>
2022-06-08 08:24:50 -0400

    Introduce formatters to platform class, starting with Asana tasks (#121)

---
+++ {{cookiecutter.project_slug}}/src/formatter.ts
@@ -0,0 +1,9 @@
+{% if cookiecutter.asana_api == 'yes' -%}
+import * as Asana from 'asana';
+
+{% endif -%}
+export default abstract class Formatter {
+{%- if cookiecutter.asana_api == 'yes' %}
+  abstract formatTask(task: Asana.resources.Tasks.Type): string;
+{% endif -%}
+}
