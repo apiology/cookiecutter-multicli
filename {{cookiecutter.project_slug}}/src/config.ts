fdd8b90191542ec2a269a535421e5d646adb9c91
Vince Broz <vince@broz.cc>
2022-06-12 11:00:27 -0400

    Add method to eagerly validate config

--- {{cookiecutter.project_slug}}/src/config.ts
+++ {{cookiecutter.project_slug}}/src/config.ts
@@ -5,4 +5,12 @@ export default abstract class Config {
   abstract fetchWorkspaceName(): Promise<string>;
 {%- endif %}
   // abstract fetchSomeConfigItem(): Promise<string>;
+
+  validate = async (): Promise<void> => {
+{%- if cookiecutter.asana_api == 'yes' %}
+    await this.fetchAsanaAccessToken();
+    await this.fetchWorkspaceName();
+{%- endif %}
+    // await fetchSomeConfigItem();
+  }
 }
