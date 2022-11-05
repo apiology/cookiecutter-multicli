8becd3477d5fd0f82396ca30e0c655c6e7f9e384
Vince Broz <vince@broz.cc>
2022-11-04 22:37:07 -0400

    Make test code more generic

--- {{cookiecutter.project_slug}}/src/chrome-extension/omnibox.test.ts
+++ {{cookiecutter.project_slug}}/src/chrome-extension/omnibox.test.ts
@@ -13,12 +13,12 @@ afterEach(() => {
 test('omniboxInputEnteredListenerNonDefault', async () => {
   const mockActOnInputData = jest.mocked(actOnInputData);
   const mockLogSuccess = jest.mocked(logSuccess);
-  const task = {} as Asana.resources.Tasks.Type;
-  mockActOnInputData.mockResolvedValue(task);
+  const resovledValue = 'foo';
+  mockActOnInputData.mockResolvedValue(resovledValue);
 
-  await omniboxInputEnteredListener('upvoter-for-asana:mumble');
-  expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:mumble');
-  expect(mockLogSuccess).toHaveBeenCalledWith(task);
+  await omniboxInputEnteredListener('{{cookiecutter.project_slug}}:mumble');
+  expect(mockActOnInputData).toHaveBeenCalledWith('{{cookiecutter.project_slug}}:mumble');
+  expect(mockLogSuccess).toHaveBeenCalledWith(resolvedValue);
 });
 
 test('omniboxInputEnteredListenerException', async () => {
@@ -26,8 +26,8 @@ test('omniboxInputEnteredListenerException', async () => {
   const mockLogSuccess = jest.mocked(logSuccess);
   mockActOnInputData.mockRejectedValue('123');
 
-  await expect(omniboxInputEnteredListener('upvoter-for-asana:mumble')).rejects.toMatch('123');
-  expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:mumble');
+  await expect(omniboxInputEnteredListener('{{cookiecutter.project_slug}}:mumble')).rejects.toMatch('123');
+  expect(mockActOnInputData).toHaveBeenCalledWith('{{cookiecutter.project_slug}}:mumble');
   expect(mockLogSuccess).not.toHaveBeenCalled();
 });
 
@@ -36,8 +36,8 @@ test('omniboxInputEnteredListenerExceptionWithAlertAvailable', async () => {
   const mockLogSuccess = jest.mocked(logSuccess);
   mockActOnInputData.mockRejectedValue('123');
 
-  await expect(omniboxInputEnteredListener('upvoter-for-asana:mumble')).rejects.toMatch('123');
-  expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:mumble');
+  await expect(omniboxInputEnteredListener('{{cookiecutter.project_slug}}:mumble')).rejects.toMatch('123');
+  expect(mockActOnInputData).toHaveBeenCalledWith('{{cookiecutter.project_slug}}:mumble');
   expect(mockLogSuccess).not.toHaveBeenCalled();
 });
 
@@ -60,12 +60,12 @@ test('omniboxInputEnteredListenerDefaultMultipleItems', async () => {
   const mockPullSuggestions = jest.mocked(pullSuggestions);
   const task = {} as Asana.resources.Tasks.Type;
   mockActOnInputData.mockResolvedValue(task);
-  const item1: Suggestion = { url: 'upvoter-for-asana:foo', description: 'dfoo', text: 'Foo'};
-  const item2: Suggestion = { url: 'upvoter-for-asana:bar', description: 'dbar', text: 'Bar' };
-  const item3: Suggestion = { url: 'upvoter-for-asana:baz', description: 'dbaz', text: 'Baz' };
+  const item1: Suggestion = { url: '{{cookiecutter.project_slug}}:foo', description: 'dfoo', text: 'Foo'};
+  const item2: Suggestion = { url: '{{cookiecutter.project_slug}}:bar', description: 'dbar', text: 'Bar' };
+  const item3: Suggestion = { url: '{{cookiecutter.project_slug}}:baz', description: 'dbaz', text: 'Baz' };
   mockPullSuggestions.mockResolvedValue([item1, item2, item3]);
 
   await omniboxInputEnteredListener('foo');
-  expect(mockActOnInputData).toHaveBeenCalledWith('upvoter-for-asana:foo');
+  expect(mockActOnInputData).toHaveBeenCalledWith('{{cookiecutter.project_slug}}:foo');
   expect(mockLogSuccess).toHaveBeenCalledWith(task);
 });
