c43822017aa0f89b736c26bf1dd896d8f400d63b
Vince Broz <vince@broz.cc>
2023-02-17 21:08:32 -0500

    Add missing import

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.ts
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.ts
@@ -3,6 +3,7 @@
  *
  * {{ cookiecutter.project_short_description }}
  */
+import { platform } from './platform.js';
 
 export const doWork = (tab: chrome.tabs.Tab) => {
   // No tabs or host permissions needed!
