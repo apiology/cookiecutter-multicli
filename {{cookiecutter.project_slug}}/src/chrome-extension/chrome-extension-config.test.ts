882694b696cc3edab9eeb6b89b5a18585db01f87
Vince Broz <vince@broz.cc>
2023-05-12 17:09:31 -0400

    Upgrade eslint, jest and friends

--- {{cookiecutter.project_slug}}/src/chrome-extension/chrome-extension-config.test.ts
+++ {{cookiecutter.project_slug}}/src/chrome-extension/chrome-extension-config.test.ts
@@ -8,8 +8,10 @@ test('create class', () => {
 });
 {%- if cookiecutter.asana_api == 'yes' %}
 test('fetchAsanaAccessToken', async () => {
-  const fakeChromeStorageSyncGet = (keys: string | string[] | { [key: string]: object } | null,
-    callback: (items: { [key: string]: object | string }) => void): void => {
+  const fakeChromeStorageSyncGet = (
+    keys: string | string[] | { [key: string]: object } | null,
+    callback: (items: { [key: string]: object | string }) => void
+  ): void => {
     expect(keys).toEqual(['asanaAccessToken']);
     callback({ asanaAccessToken: '123' });
   };
